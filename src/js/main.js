dbcd82220e5893fe4816e32f36e8e9d4a36b80ed
Juan Diego <juandiem@ucm.es>
2018-10-24 17:22:28 +0200

    Early Menu scene created

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,17 @@
 'use strict';
 
 var PlayScene = require('./play_scene.js');
+var MenuScene = require ('./menu_scene');
 
 
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+    this.game.load.image('ball', 'Assets/Sprites/Ball_Spritesheet.png');
   },
 
   create: function () {
-    this.game.state.start('preloader');
-  }
-};
-
-
-var PreloaderScene = {
-  preload: function () {
-    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
-    this.loadingBar.anchor.setTo(0, 0.5);
-    this.load.setPreloadSprite(this.loadingBar);
-
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
-  },
-
-  create: function () {
-    this.game.state.start('play');
+    this.game.state.start('menu');
   }
 };
 
@@ -35,7 +20,7 @@ window.onload = function () {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
   game.state.add('boot', BootScene);
-  game.state.add('preloader', PreloaderScene);
+  game.state.add('menu', MenuScene);
   game.state.add('play', PlayScene);
 
   game.state.start('boot');
---
+++ src/js/menu_scene.js
@@ -0,0 +1,26 @@
+'use strict';
+var MenuScene = {
+    preload: function () {
+      this.game.load.spritesheet('ball', 'Assets/Sprites/Ball_Spritesheet.png',1000,1000);
+    },
+  
+    create: function () {
+      this.game.stage.backgroundColor = "#105987";
+      
+      var buttonball = this.createButton(this.game.world.centerX, this.game.world.centerY,
+        'ball',this.actionOnClick,this,4,0,11);
+    },
+    createButton: function(x,y,avatar,funct,instance,overfr,outfr,downfr){
+      var avatarButton = this.game.add.button(
+        x,y,avatar,funct,instance,overfr,outfr,downfr);
+      avatarButton.scale.setTo(0.1);
+      avatarButton.anchor.setTo(0.5,0.5);
+     },
+  
+     actionOnClick: function(){
+      console.log("Por fin Coño de la madre");
+      this.game.state.start('play');
+     }
+  };
+
+  module.exports = MenuScene;
